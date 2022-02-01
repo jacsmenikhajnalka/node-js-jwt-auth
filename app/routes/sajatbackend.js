8d5510c849934351dc571133407ca49326858cca
tanulo <tanulo@barossgabor.local>
2022-02-01 13:16:57 +0100

    0201

--- app/routes/sajatbackend.js
+++ app/routes/sajatbackend.js
@@ -98,7 +98,7 @@ module.exports = function(app) {
     
     connection.connect()
     
-    connection.query("INSERT INTO `konyv` (`konyv_id`, `konyv_nev`, `konyv_tipus`, `konyv_kep`) VALUES (NULL, 'a', '1', '1');", function (err, rows, fields) {
+    connection.query("INSERT INTO `konyv` (`konyv_id`, `konyv_nev`, `konyv_tipus`, `konyv_kep`) VALUES (NULL,'"+req.body.bevitel+"' );", function (err, rows, fields) {
       if (err) throw err
     
       console.log(rows)
