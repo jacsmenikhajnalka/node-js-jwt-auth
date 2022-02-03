700260b776710c15be1502f51e98487ed6f251a0
tanulo <tanulo@barossgabor.local>
2022-02-03 12:41:03 +0100

    0203

--- app/routes/sajatbackend.js
+++ app/routes/sajatbackend.js
@@ -1,5 +1,6 @@
 const { authJwt } = require("../middleware");
 const controller = require("../controllers/user.controller");
+const fileupload = require("express-fileupload");
 
 module.exports = function(app) {
   app.use(function(req, res, next) {
@@ -107,6 +108,44 @@ module.exports = function(app) {
     
     connection.end()
   })
+
+  app.post('/felvitel2', (req, res) => {
+
+    var mysql = require('mysql')
+    var connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      database: 'konyv_adatb'
+    })
+
+    
+    
+    connection.connect()
+    
+    connection.query("INSERT INTO konyv  VALUES (NULL, '"+req.body.bevitel1+"', '' ,'')", function (err, rows, fields) {
+      if (err) throw err
+    
+      console.log(rows)
+      res.send(rows)
+    })
+    
+    connection.end()
+  })
+
+  app.use(fileupload());
+  app.post("/upload", (req, res) => {
+    const newpath = "./kepek/";
+    const file = req.files.file;
+    const filename = file.name;
+  
+    file.mv(`${newpath}${filename}`, (err) => {
+      if (err) {
+        return res.status(500).send({ message: "File upload failed", code: 200 });
+      }
+        return res.status(200).send({ message: "File Uploaded", code: 200 });
+    });
+  });
 
 
   app.post('/torleskonyv', (req, res) => {
